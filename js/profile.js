d04402dac86bcdf01843eaf72b7c5f6b90286dc3
Radoslav Karatanev <rmkrmkrmk13@gmail.com>
2025-01-27 21:11:15 +0200

    Minor grammar fix

--- js/profile.js
+++ js/profile.js
@@ -2,7 +2,7 @@ fetch('php/profile.php')
     .then(response => response.json())
     .then(data => {
         document.getElementById('username').textContent = data.username;
-        document.getElementById('email').textContent = 'Email: ' + data.email;
+        document.getElementById('email').textContent = data.email;
     })
     .catch(error => {
         console.error('Error fetching profile data:', error);
