54d43c846c0c6ba3ae65cf985ce2fb7f3a5db3ff
Radoslav Karatanev <rmkrmkrmk13@gmail.com>
2025-01-27 23:10:39 +0200

    Update database script

--- db/videoannotator.sql
+++ db/videoannotator.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 26, 2025 at 08:41 PM
+-- Generation Time: Jan 27, 2025 at 10:05 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -23,6 +23,27 @@ SET time_zone = "+00:00";
 
 -- --------------------------------------------------------
 
+--
+-- Table structure for table `subtitles`
+--
+
+CREATE TABLE `subtitles` (
+  `id` int(11) NOT NULL,
+  `drive_id` varchar(100) NOT NULL,
+  `video_id` int(100) NOT NULL,
+  `format` enum('SRT','SUB','HTML','XML','JSON') NOT NULL,
+  `subtitle_name` varchar(255) NOT NULL,
+  `language` varchar(20) DEFAULT NULL,
+  `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_on` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `subtitles`
+--
+
+INSERT INTO `subtitles` (`id`, `drive_id`, `video_id`, `format`, `subtitle_name`, `language`, `created_on`, `updated_on`) VALUES
+(1, '1cWHltNXyOrAPHEoI4BE_AvmnTZabES22', 1, 'SRT', 'subtitles (1).srt', NULL, '2025-01-27 22:18:40', '2025-01-27 22:18:40');
 
 -- --------------------------------------------------------
 
@@ -31,23 +52,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `users` (
-  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `id` int(100) NOT NULL,
   `username` varchar(20) NOT NULL,
   `email` varchar(100) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
-  `drive_id` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
+  `drive_id` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`username`, `email`, `password`, `drive_id`) VALUES
-( 'radkoPiratko', 'karata', 'karaoivwnniw', 0),
-( 'gergana-pile-shareno', 'gergana.yordanovaa03@gmail.com', 'gericyrk', 0),
-( 'KaloyanTs25', 'kaloyants25@gmail.com', 'kalacyrk', 0),
-( 'kytsvetkov', 'a@a.a', 'aaaaaa', 0);
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `drive_id`) VALUES
+(1, 'admin', 'admin@gmail.com', '$2y$10$ABpgODssTYU38OcUGnDWgueu92XW2dXSXPj2XH5nTA88uV0J2bSRe', 0), -- password: admin1
+(2, 'milen123', 'milen@abv.bg', '$2y$10$konTBTE1ROVBqyIUmA101.vOcL7z/KEc654Q6.mnubC5.5ua2sHxy', 0); -- password: milenparola
 
 -- --------------------------------------------------------
 
@@ -56,50 +74,25 @@ INSERT INTO `users` (`username`, `email`, `password`, `drive_id`) VALUES
 --
 
 CREATE TABLE `videos` (
-  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `id` int(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `drive_id` varchar(100) NOT NULL,
   `owner_id` int(100) NOT NULL,
   `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_on` timestamp NOT NULL DEFAULT current_timestamp(),
-  `is_private` boolean NOT NULL DEFAULT false,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`owner_id`) REFERENCES `users`(`id`)
+  `is_private` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `videos`
 --
 
-INSERT INTO `videos` (`name`, `drive_id`, `owner_id`, `created_on`, `updated_on`, `is_private`) VALUES
-( 'dosi hbd', '1vcwdviw66B5D2GsaleMfDYdGql4ztxbG', 3, '2025-01-26 14:54:10', '2025-01-26 14:54:10', 0);
+INSERT INTO `videos` (`id`, `name`, `drive_id`, `owner_id`, `created_on`, `updated_on`, `is_private`) VALUES
+(1, 'dosi hbd', '1vcwdviw66B5D2GsaleMfDYdGql4ztxbG', 1, '2025-01-26 14:54:10', '2025-01-26 14:54:10', 0),
+(2, 'video 2', '1qY3mUv9v16yd5qXOBoX4TfD3No2zoRmH', 2, '2025-01-26 14:54:10', '2025-01-26 14:54:10', 0),
+(3, 'rubik cube', '1kf5M-VTsqZlDtwycbG68AEs27f6ehegN', 1, '2025-01-26 14:54:10', '2025-01-26 14:54:10', 0),
+(4, 'sea', '1KHPf3x7uSfPnBXaJx3bH8x7E4AopI5qi', 1, '2025-01-26 14:54:10', '2025-01-26 14:54:10', 0);
 
-
-
-
---
--- Table structure for table `subtitles`
---
-
-CREATE TABLE `subtitles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `drive_id` varchar(100) NOT NULL,
-  `video_id` int(100) NOT NULL,
-  `format` enum('SRT','SUB','HTML','XML','JSON') NOT NULL,
-  `subtitle_name` varchar(255) NOT NULL,
-  `language` varchar(20) DEFAULT NULL,
-  `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_on` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`video_id`) REFERENCES `videos`(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `subtitles`
---
-
-INSERT INTO `subtitles` ( `drive_id`, `video_id`,`subtitle_name` , `format`, `created_on`, `updated_on`) VALUES
-( '1yN0gfEE_f5GduoWpX3q7d4HXqBFZcDWL', '1', 'test','SRT', '2025-01-05 15:46:15', '2025-01-05 15:47:07');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
